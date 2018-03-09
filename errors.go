ff21b2d44362c0eda035dce9f974b56070e295b9
reficul31 <reficul31@gmail.com>
2018-03-09 11:13:05 +0530

    Defining the errors

---
+++ errors.go
@@ -0,0 +1,11 @@
+package main
+
+import "errors"
+
+var (
+	// ErrUndefinedAlgorithm is returned when an undefined algorithm type is found
+	ErrUndefinedAlgorithm = errors.New("Undefined hash algorithm")
+
+	// ErrUndefinedEncoding is returned when an undefined encoder type is encountered
+	ErrUndefinedEncoding = errors.New("Undefined encoding")
+)
