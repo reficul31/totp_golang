d35b7fd60ca86ccd06b863be9a4361aa2af0e059
reficul31 <reficul31@gmail.com>
2018-03-09 11:13:46 +0530

    Making the main functions for TOTP generation and verification

---
+++ totp.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"time"
+	"strings"
+	"crypto/hmac"
+)
+
+// ValidateTOTP validates the expectedTOTP with the generatedTOTP
+func ValidateTOTP(expectedTOTP string) (bool, error) {
+	opts, err := PopulateAuthOpts()
+	if err != nil {
+		return false, err 
+	}
+
+	generatedTOTP, err := GenerateTOTP(opts)
+	if err != nil {
+		return false, err
+	}
+
+	return strings.Compare(expectedTOTP, generatedTOTP) == 0, nil
+}
+
+// GenerateTOTP generates a TOTP based upon the AuthOpts provided
+func GenerateTOTP(opts AuthOpts) (string, error) {
+	counter := time.Now().Second() / opts.Period
+	
+	algo, err := ReturnHash(opts.Algo)
+	if err != nil {
+		return "", err
+	}
+
+	mac := hmac.New(algo, StringToBytes(opts.Secret))
+	mac.Write(StringToBytes(string(counter)))
+
+	return EncodeToString(mac.Sum(nil), opts)
+}
